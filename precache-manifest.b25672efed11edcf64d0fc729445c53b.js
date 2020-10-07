b5a7c694471d77e0beb540d2fd7ea4b05607810f
lelchek <olga.dyachek@gmail.com>
2020-10-07 18:26:03 +0300

    Updates

---
+++ precache-manifest.b25672efed11edcf64d0fc729445c53b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6453bf06ed8d5f8717513e24b9bcce92",
+    "url": "/millionaire/index.html"
+  },
+  {
+    "revision": "6256ca98b7b538442d52",
+    "url": "/millionaire/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "12e715208f3de6cedc37",
+    "url": "/millionaire/static/js/2.c3a85ea9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/millionaire/static/js/2.c3a85ea9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6256ca98b7b538442d52",
+    "url": "/millionaire/static/js/main.b778e6f8.chunk.js"
+  },
+  {
+    "revision": "078e70291101148e5f5b",
+    "url": "/millionaire/static/js/runtime-main.70d563fc.js"
+  },
+  {
+    "revision": "3e2e3523aff3f5287308b3d76e9c020e",
+    "url": "/millionaire/static/media/logoXl.3e2e3523.png"
+  }
+]);
